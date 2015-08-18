7dd6495f02d659a67244e8b7803d998ea0b15df3
Mathilde Lefrancois <mathilde@kwaala.fr>
2015-08-18 16:47:59 +0200

    working without css

--- task-manager/db/seeds.rb
+++ task-manager/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+tasks = Task.create([{ name: 'Coder', description: 'Apprendre au Wagon', importance: 4 }])
